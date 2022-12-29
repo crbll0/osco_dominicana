514a88364b8792128deffe30a9808696e45f3f25
TheHardest18 <isias1626@gmail.com>
2022-12-29 13:15:15 -0400

    [UPD] custom_reports_m2

--- custom_reports_m2/__manifest__.py
+++ custom_reports_m2/__manifest__.py
@@ -17,7 +17,8 @@
     'depends': ['base', 'account', 'l10n_do_accounting', 'stock'],
     'data': [
         # 'security/ir.model.access.csv',
-        'views/paper_format.xml',
+        #'views/paper_format.xml',
+        'views/a5_format.xml',
         'views/picking_report.xml',
         'views/payment_report.xml',
 	# 'views/account_move_inherit.xml',
