b737321ccc571c317257e356c2b6e68015637366
TheHardest18 <isias1626@gmail.com>
2022-12-29 12:03:23 -0400

    [FIX] custom paper format

--- custom_reports_m2/__manifest__.py
+++ custom_reports_m2/__manifest__.py
@@ -18,7 +18,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         #'views/paper_format.xml',
-        'views/custom_format_a5',
+        'views/custom_format_a5.xml',
         'views/picking_report.xml',
         'views/payment_report.xml',
 	# 'views/account_move_inherit.xml',
